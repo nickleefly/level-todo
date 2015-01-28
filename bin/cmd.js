e99b9a3fd197f510e3602aabf0a3190a3f165175
Xiuyu Li <nickleefly@gmail.com>
2015-01-28 12:31:17 +0800

    fix path

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,20 +4,20 @@ var path = require('path')
 var mkdirp = require('mkdirp')
 var minimist = require('minimist')
 var levelup = require('levelup')
- 
+
 var argv = minimist(process.argv.slice(2))
 var HOME = process.env.HOME
 var dbDir = argv.d || path.join(HOME, 'todo.db')
 mkdirp.sync(dbDir)
- 
+
 var db = levelup(dbDir, { encoding: 'json' })
 var todo = require('../index')
- 
+
 function printUsage() {
-  fs.createReadStream(__dirname + '../usage.md')
+  fs.createReadStream(__dirname + '/../usage.md')
   .pipe(process.stdout)
 }
- 
+
 var name = argv._[1]
 var value = argv._[2]
 if (argv.h) {
