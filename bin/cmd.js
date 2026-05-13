1e4b9ab77a2714fa2814379b068f000c41067b54
Xiuyu Li <nickleefly@gmail.com>
2026-05-13 10:39:50 +0800

    Upgrade deprecated Level.js dependencies
    Replace leveldown + levelup with modern level package (v10).
    Update minimist to 1.2.8 (security fix) and mkdirp to 3.0.1.
    Migrate code from callback-based to async/await API.

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,40 +3,46 @@ var fs = require('fs')
 var path = require('path')
 var mkdirp = require('mkdirp')
 var minimist = require('minimist')
-var levelup = require('levelup')
-var leveldown = require('leveldown')
+var { Level } = require('level')
 
 var argv = minimist(process.argv.slice(2))
 var HOME = process.env.HOME
 var dbDir = path.join(HOME, 'todo.db')
 mkdirp.sync(dbDir)
 
-var db = levelup(leveldown(dbDir))
-var todo = require('../index')
+async function main () {
+  var db = new Level(dbDir)
+  var todo = require('../index')
 
-function printUsage () {
-  fs.createReadStream(__dirname + '/../usage.md')
-    .pipe(process.stdout)
+  function printUsage () {
+    fs.createReadStream(__dirname + '/../usage.md')
+      .pipe(process.stdout)
+  }
+
+  var name = argv._[1]
+  var value = argv._[2]
+  if (argv.h) {
+    printUsage()
+  } else if (argv._[0] === 'add') {
+    await todo.add(db, name, value)
+  } else if (argv._[0] === 'get') {
+    await todo.get(db, name)
+  } else if (argv._[0] === 'rm' || argv._[0] === 'done') {
+    await todo.del(db, name)
+  } else if (argv._[0] === 'list') {
+    await todo.list(db)
+  } else if (argv._[0] === 'destroy') {
+    await db.close()
+    await Level.destroy(dbDir)
+    console.log('todo list database destroied!')
+  } else {
+    printUsage()
+  }
+
+  await db.close()
 }
 
-var name = argv._[1]
-var value = argv._[2]
-if (argv.h) {
-  printUsage()
-} else if (argv._[0] === 'add') {
-  todo.add(db, name, value)
-} else if (argv._[0] === 'get') {
-  todo.get(db, name)
-} else if (argv._[0] === 'rm' || argv._[0] === 'done') {
-  todo.del(db, name)
-} else if (argv._[0] === 'list') {
-  todo.list(db)
-} else if (argv._[0] === 'destroy') {
-  db.close(function () {
-    require('leveldown').destroy(dbDir, function (err) {
-      console.log('todo list database destroied!')
-    })
-  })
-} else {
-  printUsage()
-}
+main().catch(function (err) {
+  console.error('Ooops!', err)
+  process.exit(1)
+})
--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 var todo = exports
 var util = require('util')
 
-todo.add = function (db, name, value) {
-  db.put(name, value, function (err) {
-    if (err) return console.log('Ooops!', err)
-  })
+todo.add = async function (db, name, value) {
+  try {
+    await db.put(name, value)
+  } catch (err) {
+    console.log('Ooops!', err)
+  }
 }
 
-todo.get = function (db, name) {
-  db.get(name, function (err, value) {
-    if (err) return console.log('Ooops, no given key founded')
+todo.get = async function (db, name) {
+  try {
+    var value = await db.get(name)
     console.log(util.format('value for %s: %s', name, value))
-  })
+  } catch (err) {
+    console.log('Ooops, no given key founded')
+  }
 }
 
-todo.del = function (db, name) {
-  db.del(name, function (err) {
-    if (err) return console.log('Ooops!', err)
-    console.log('key \"' + name + '\" is finished')
-  })
+todo.del = async function (db, name) {
+  try {
+    await db.del(name)
+    console.log('key "' + name + '" is finished')
+  } catch (err) {
+    console.log('Ooops!', err)
+  }
 }
 
-todo.list = function (db) {
-  db.createReadStream()
-    .on('data', function (data) {
-      console.log(util.format('value for %s: %s', data.key, data.value))
-    })
+todo.list = async function (db) {
+  try {
+    var it = db.iterator()
+    var entry = await it.next()
+    while (entry !== undefined) {
+      console.log(util.format('value for %s: %s', entry[0], entry[1]))
+      entry = await it.next()
+    }
+  } catch (err) {
+    console.log('Ooops!', err)
+  }
 }
